6109561472b3fcba966e7ae08163cd852b17493c
K-Krut <katiakrut448@gmail.com>
2025-08-01 11:42:23 +0300

    feat/DFS-197: changed endpoints in frontend

--- frontend/src/lib/api/orbit-ml-models/index.ts
+++ frontend/src/lib/api/orbit-ml-models/index.ts
@@ -9,32 +9,32 @@ export class MlModelsApi {
   }
 
   async createModel(organizationId: number, orbitId: number, collectionId: number, data: MlModelCreator) {
-    const { data: responseData } = await this.api.post<CreateModelResponse>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models`, data)
+    const { data: responseData } = await this.api.post<CreateModelResponse>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts`, data)
     return responseData
   }
 
   async getModelsList(organizationId: number, orbitId: number, collectionId: number) {
-    const { data: responseData } = await this.api.get<MlModel[]>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models`)
+    const { data: responseData } = await this.api.get<MlModel[]>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts`)
     return responseData
   }
 
   async updateModel(organizationId: number, orbitId: number, collectionId: number, modelId: number, data: UpdateMlModelPayload) {
-    const { data: responseData } = await this.api.patch<MlModel>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models/${modelId}`, data)
+    const { data: responseData } = await this.api.patch<MlModel>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts/${modelId}`, data)
     return responseData
   }
 
   async getModelDownloadUrl(organizationId: number, orbitId: number, collectionId: number, modelId: number) {
-    const { data: responseData } = await this.api.get<{ url: string }>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models/${modelId}/download-url`)
+    const { data: responseData } = await this.api.get<{ url: string }>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts/${modelId}/download-url`)
     return responseData
   }
 
   async getModelDeleteUrl(organizationId: number, orbitId: number, collectionId: number, modelId: number) {
-    const { data: responseData } = await this.api.get<{ url: string }>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models/${modelId}/delete-url`)
+    const { data: responseData } = await this.api.get<{ url: string }>(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts/${modelId}/delete-url`)
     return responseData
   }
 
   async confirmModelDelete(organizationId: number, orbitId: number, collectionId: number, modelId: number) {
-    const { data: responseData } = await this.api.delete(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/ml-models/${modelId}`)
+    const { data: responseData } = await this.api.delete(`/organizations/${organizationId}/orbits/${orbitId}/collections/${collectionId}/model_artifacts/${modelId}`)
     return responseData
   }
 }
