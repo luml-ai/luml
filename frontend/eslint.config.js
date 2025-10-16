653de28762c677c4f6fefb427c2fa9e2815c3ce7
Veronica <cattymew13@gmail.com>
2025-10-16 12:10:00 +0300

    feat/DFS-382: add storybook

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,20 +1,14 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  ...pluginVue.configs['flat/essential'],
-  ...defineConfigWithVueTs(),
-  ...vueTsConfigs(),
-  skipFormatting,
-]
+export default [{
+  name: 'app/files-to-lint',
+  files: ['**/*.{ts,mts,tsx,vue}'],
+}, {
+  name: 'app/files-to-ignore',
+  ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+}, ...pluginVue.configs['flat/essential'], ...defineConfigWithVueTs(), ...vueTsConfigs(), skipFormatting, ...storybook.configs["flat/recommended"]];
