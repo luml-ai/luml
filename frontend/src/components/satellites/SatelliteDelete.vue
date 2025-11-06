39190e21d4caab9d9a71cfdd4fb83b52d1a27eb8
Veronica <cattymew13@gmail.com>
2025-11-06 18:28:02 +0200

    fix/DFS-435: add confirmation window for satellites

---
+++ frontend/src/components/satellites/SatelliteDelete.vue
@@ -0,0 +1,83 @@
+<template>
+  <Dialog
+    v-model:visible="visible"
+    modal
+    :draggable="false"
+    header="Delete this satellite?"
+    :style="{ width: '350px' }"
+  >
+    <div>
+      <p class="text">This action will unpair the satellite {{ name }} from the platform.</p>
+      <div class="checkbox-wrapper">
+        <Checkbox v-model="accept" inputId="deleteAccept" binary />
+        <label for="deleteAccept">Yes, delete this satellite</label>
+      </div>
+    </div>
+    <template #footer>
+      <Button @click="visible = false" :disabled="loading">cancel</Button>
+      <Button
+        severity="warn"
+        outlined
+        :disabled="!accept"
+        :loading="loading"
+        @click="deleteSatellite"
+      >
+        delete
+      </Button>
+    </template>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Dialog, Button, Checkbox, useToast } from 'primevue'
+import { useSatellitesStore } from '@/stores/satellites'
+import { simpleErrorToast, simpleSuccessToast } from '@/lib/primevue/data/toasts'
+import { getErrorMessage } from '@/helpers/helpers'
+
+type Props = {
+  organizationId: string
+  orbitId: string
+  satelliteId: string
+  name: string
+}
+
+type Emits = {
+  delete: []
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<Emits>()
+
+const visible = defineModel<boolean>('visible')
+const toast = useToast()
+const satellitesStore = useSatellitesStore()
+const loading = ref(false)
+const accept = ref(false)
+
+async function deleteSatellite() {
+  try {
+    loading.value = true
+    await satellitesStore.deleteSatellite(props.organizationId, props.orbitId, props.satelliteId)
+    toast.add(simpleSuccessToast(`Satellite "${props.name}" deleted successfully.`))
+    emits('delete')
+    visible.value = false
+  } catch (e: any) {
+    toast.add(simpleErrorToast(getErrorMessage(e, 'Could not delete satellite')))
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<style scoped>
+.text {
+  margin-bottom: 14px;
+}
+
+.checkbox-wrapper {
+  display: flex;
+  align-items: center;
+  gap: 7px;
+}
+</style>
