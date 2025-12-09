1dd7c4fa11430e2d85220fbfcfd5e46e11c36732
Oleh Kostromin <kostromin97@gmail.com>
2025-12-09 21:07:49 +0100

    renamed documentation to luml

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -6,15 +6,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config: Config = {
-  title: 'DataForce Studio',
+  title: 'LUML',
   tagline: 'Build AI Solutions Faster than Ever',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://gist.githubusercontent.com/OKUA1/1d730de58b9c7ccc3010d4e118552c5d/raw/e6d2b6ff16759b6e2021e1a57e0427c722dd5adc/luml_logo_mark_black.svg',
 
-  url: process.env.URL || 'https://dataforce.studio',
+  url: process.env.URL || 'https://luml.ai',
   baseUrl: '/',
 
-  organizationName: 'DataForce', 
-  projectName: 'DataForce Studio', 
+  organizationName: 'LUML', 
+  projectName: 'LUML', 
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
@@ -45,11 +45,12 @@ headTags: [],
   themeConfig: {
     image: 'img/dsf.webp',
     navbar: {
-      title: 'DataForce Studio', 
+      title: '', 
       
       logo: {
-        alt: 'DataForce Studio Logo',
-        src: 'img/logo.png',
+        alt: 'LUML Logo',
+        src: 'https://gist.githubusercontent.com/OKUA1/1d730de58b9c7ccc3010d4e118552c5d/raw/379005eb32b69b6281e2c0be70fd82e5ef7bd456/luml_logo_full_black.svg',
+        srcDark: 'https://gist.githubusercontent.com/OKUA1/1d730de58b9c7ccc3010d4e118552c5d/raw/379005eb32b69b6281e2c0be70fd82e5ef7bd456/luml_logo_full_white.svg',
         href: '/', 
       },
       items: [
@@ -81,32 +82,32 @@ headTags: [],
           items: [
             {
               label: 'Documentation',
-              to: '/documentation/intro', 
+              to: '/documentation/quickstart', 
             },
             {
               label: 'Guides',
-              to: '/guides/getting-started', 
+              to: '/guides/user_guidline', 
             },
             {
               label: 'SDK',
-              to: '/sdk/reference/dataforce/api', 
+              to: '/sdk/bucket-secrets', 
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} DataForce Solutions GmbH. All rights reserved.`,
+      copyright: `Copyright © ${new Date().getFullYear()} LUML. All rights reserved.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
-    announcementBar: {
-      id: 'wip_notice', 
-      content: '🚧 This documentation is a work in progress.',
-      backgroundColor: '#fff3cd', 
-      textColor: '#663c00',       
-      isCloseable: true,
-    },
+    // announcementBar: {
+    //   id: 'wip_notice', 
+    //   content: '🚧 This documentation is a work in progress.',
+    //   backgroundColor: '#fff3cd', 
+    //   textColor: '#663c00',       
+    //   isCloseable: true,
+    // },
   } satisfies Preset.ThemeConfig,
 };
 
