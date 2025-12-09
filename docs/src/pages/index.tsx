0199f96953424ca6283afd66cdc7b92f527365fd
Oleh Kostromin <kostromin97@gmail.com>
2025-12-09 21:07:49 +0100

    renamed documentation to luml

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,41 +1,5 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import {Redirect} from '@docusaurus/router';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/getting-started">
-            Documentation
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title} docs`}
-      description="Dataforce Studio documentation">
-      <HomepageHeader />
-    </Layout>
-  );
+export default function Home() {
+  return <Redirect to="/documentation/quickstart" />;
 }
