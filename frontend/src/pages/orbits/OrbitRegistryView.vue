76037d76e0d2684048448559d0b33ce6b24a59d2
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2025-12-19 19:44:46 +0200

    feat/DFS-534: add collection skeleton

--- frontend/src/pages/orbits/OrbitRegistryView.vue
+++ frontend/src/pages/orbits/OrbitRegistryView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div v-if="!loading">
+  <div v-if="loading" class="loading-container">
+    <Skeleton v-for="i in 10" :key="i" style="height: 146.5px" />
+  </div>
+  <div v-else>
     <div v-if="!collectionsStore.collectionsList.length" class="content">
       <Folders :size="35" color="var(--p-primary-color)" />
       <h3 class="label">Welcome to the Registry</h3>
@@ -22,28 +25,30 @@
     :organization-id="orbitsStore.currentOrbitDetails!.organization_id"
     :orbit-id="orbitsStore.currentOrbitDetails!.id"
     :visible="collectionsStore.creatorVisible"
-    @update:visible="
-      (val) => (val ? collectionsStore.showCreator() : collectionsStore.hideCreator())
-    "
+    @update:visible="updateCreatorVisible"
   />
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount, onUnmounted, ref } from 'vue'
-import { useToast } from 'primevue'
+import { Skeleton, useToast } from 'primevue'
 import { Folders } from 'lucide-vue-next'
 import { useOrbitsStore } from '@/stores/orbits'
 import { simpleErrorToast } from '@/lib/primevue/data/toasts'
 import { useCollectionsStore } from '@/stores/collections'
+import { PermissionEnum } from '@/lib/api/DataforceApi.interfaces'
 import CollectionsList from '@/components/orbits/tabs/registry/CollectionsList.vue'
 import CollectionCreator from '@/components/orbits/tabs/registry/CollectionCreator.vue'
-import { PermissionEnum } from '@/lib/api/DataforceApi.interfaces'
 
 const orbitsStore = useOrbitsStore()
 const collectionsStore = useCollectionsStore()
 const toast = useToast()
 
 const loading = ref(false)
+
+function updateCreatorVisible(visible: boolean | undefined) {
+  visible ? collectionsStore.showCreator() : collectionsStore.hideCreator()
+}
 
 onBeforeMount(async () => {
   try {
@@ -81,5 +86,10 @@ onUnmounted(() => {
 .text {
   font-size: 12px;
   color: var(--p-text-muted-color);
+}
+.loading-container {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 }
 </style>
