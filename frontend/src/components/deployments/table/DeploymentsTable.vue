49941251d4bdcc5612f8198960712789a5fb8559
K-Krut <katiakrut448@gmail.com>
2026-01-07 23:04:21 +0200

    feat: add "not_responding" status handling in deployments

--- frontend/src/components/deployments/table/DeploymentsTable.vue
+++ frontend/src/components/deployments/table/DeploymentsTable.vue
@@ -45,8 +45,17 @@
           <div class="cell">
             <Tag v-if="data.status === DeploymentStatusEnum.active" severity="success">Active</Tag>
             <Tag v-if="data.status === DeploymentStatusEnum.pending" severity="warn"> Pending </Tag>
-            <div v-if="data.status === DeploymentStatusEnum.failed" class="tag-with-icon">
-              <Tag severity="danger">Failed</Tag>
+            <div
+              v-if="
+                data.status === DeploymentStatusEnum.failed ||
+                data.status === DeploymentStatusEnum.not_responding
+              "
+              class="tag-with-icon"
+            >
+              <Tag severity="danger">{{
+                data.status === DeploymentStatusEnum.failed ? 'Failed' : 'Not Responding'
+              }}</Tag>
+
               <TriangleAlert
                 v-if="data.error_message"
                 v-tooltip.top="'Show error'"
