1b2a20aed95dda7100c5178fd9c73e39e541d3be
Limanskiy <102548243+limanski-valera@users.noreply.github.com>
2026-02-04 17:13:10 +0200

    feat/LM-171: lumlflow main screen (#318)

--- lumlflow/frontend/src/main.ts
+++ lumlflow/frontend/src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/main.css'
+import { createPinia } from 'pinia'
+import { PrimeVueProvider } from '@/app/providers/prime-vue'
+import { ConfirmationService, Tooltip, ToastService } from 'primevue'
+import App from '@/app/App.vue'
+import router from '@/router'
+import '@/assets/css/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVueProvider)
+app.use(ConfirmationService)
+app.use(ToastService)
+
+app.directive('tooltip', Tooltip)
+
+app.mount('#app')
