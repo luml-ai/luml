d9df4657767ba1edeb86afee25f1d93122d235cc
Limanskiy <102548243+limanski-valera@users.noreply.github.com>
2026-02-25 20:10:47 +0200

    feat/LM-351: add experiment overview (#358)

---
+++ lumlflow/frontend/src/components/upload/data.ts
@@ -0,0 +1,10 @@
+import type { DialogPassThroughOptions } from 'primevue'
+
+export const DIALOG_PT: DialogPassThroughOptions = {
+  root: {
+    class: 'w-[500px]',
+  },
+  header: {
+    class: 'text-xl uppercase',
+  },
+}
--- lumlflow/frontend/src/router/router.const.ts
+++ lumlflow/frontend/src/router/router.const.ts
@@ -1,9 +1,22 @@
 export const ROUTE_NAMES = {
   HOME: 'home',
   EXPERIMENT: 'experiment',
+  EXPERIMENT_DETAILS: 'experiment-details',
+  EXPERIMENT_OVERVIEW: 'experiment-overview',
+  EXPERIMENT_METRICS: 'experiment-metrics',
+  EXPERIMENT_TRACES: 'experiment-traces',
+  EXPERIMENT_EVALS: 'experiment-evals',
+  EXPERIMENT_ATTACHMENTS: 'experiment-attachments',
 }
 
 export const ROUTES = {
   [ROUTE_NAMES.HOME]: '/',
   [ROUTE_NAMES.EXPERIMENT]: 'experiments/:groupId',
+
+  [ROUTE_NAMES.EXPERIMENT_DETAILS]: 'experiments/:groupId/:experimentId',
+  [ROUTE_NAMES.EXPERIMENT_OVERVIEW]: '',
+  [ROUTE_NAMES.EXPERIMENT_METRICS]: 'metrics',
+  [ROUTE_NAMES.EXPERIMENT_TRACES]: 'traces',
+  [ROUTE_NAMES.EXPERIMENT_EVALS]: 'evals',
+  [ROUTE_NAMES.EXPERIMENT_ATTACHMENTS]: 'attachments',
 }
