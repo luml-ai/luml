f7e709e4e52315481d1bd86d50862b9075856587
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2026-03-01 16:13:59 +0200

    chore: fix lumlflow build

--- lumlflow/frontend/src/components/upload/data.ts
+++ lumlflow/frontend/src/components/upload/data.ts
@@ -1,4 +1,8 @@
+import { zodResolver } from '@primevue/forms/resolvers/zod'
 import type { DialogPassThroughOptions } from 'primevue'
+import z from 'zod'
+
+type Resolver = ReturnType<typeof zodResolver>
 
 export const DIALOG_PT: DialogPassThroughOptions = {
   root: {
@@ -8,3 +12,47 @@ export const DIALOG_PT: DialogPassThroughOptions = {
     class: 'text-xl uppercase',
   },
 }
+
+export const resolver: Resolver = zodResolver(
+  z
+    .object({
+      type: z.enum(['auto', 'model', 'experiment']),
+      organization: z.string().nullable(),
+      orbit: z.string().nullable(),
+      collection: z.string().nullable(),
+      name: z.string().min(3).max(255),
+      description: z
+        .string()
+        .max(255)
+        .refine((val) => !val || val.length >= 3, {
+          message: 'Description must be at least 3 characters if not empty',
+        }),
+      tags: z.array(z.string().min(3).max(255)),
+      embedExperiment: z.boolean(),
+    })
+    .superRefine((data, ctx) => {
+      if (!data.organization) {
+        ctx.addIssue({
+          path: ['organization'],
+          code: z.ZodIssueCode.custom,
+          message: 'Organization is required',
+        })
+      }
+
+      if (data.organization && !data.orbit) {
+        ctx.addIssue({
+          path: ['orbit'],
+          code: z.ZodIssueCode.custom,
+          message: 'Orbit is required',
+        })
+      }
+
+      if (data.orbit && !data.collection) {
+        ctx.addIssue({
+          path: ['collection'],
+          code: z.ZodIssueCode.custom,
+          message: 'Collection is required',
+        })
+      }
+    }),
+)
