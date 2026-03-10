a0786fbe8942a446219042b954d3d03fb8a4f846
K-Krut <katiakrut448@gmail.com>
2026-03-10 13:57:16 +0200

    feat: moved annotations endpoints to separate module

--- lumlflow/lumlflow/service.py
+++ lumlflow/lumlflow/service.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
 
+from lumlflow.api.annotations import annotations_router
 from lumlflow.api.auth import auth_router
 from lumlflow.api.experiment_groups import experiment_groups_router
 from lumlflow.api.experiments import experiments_router
@@ -22,6 +23,7 @@ class AppService(FastAPI):
         self.include_router(router=experiment_groups_router)
         self.include_router(router=experiments_router)
         self.include_router(router=models_router)
+        self.include_router(router=annotations_router)
         self.include_error_handlers()
         self.custom_openapi()
 
