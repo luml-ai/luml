50d64e4e95030eb8ee12eb4f65b0615bc102a664
K-Krut <katiakrut448@gmail.com>
2026-04-21 12:56:52 +0300

    feat: added get_model_details endpoint

--- lumlflow/lumlflow/api/models.py
+++ lumlflow/lumlflow/api/models.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status
 
 from lumlflow.handlers.models import ModelsHandler
-from lumlflow.schemas.models import Model, UpdateModel
+from lumlflow.schemas.models import Model, ModelDetails, UpdateModel
 
 models_router = APIRouter(prefix="/api/models", tags=["models"])
 models_handler = ModelsHandler()
@@ -15,3 +15,8 @@ def update_model(model_id: str, body: UpdateModel) -> Model:
 @models_router.delete("/{model_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_model(model_id: str) -> None:
     models_handler.delete_model(model_id)
+
+
+@models_router.get("/{model_id}", response_model=ModelDetails)
+def get_model_details(model_id: str) -> ModelDetails:
+    return models_handler.get_model_details(model_id)
--- lumlflow/lumlflow/schemas/models.py
+++ lumlflow/lumlflow/schemas/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from lumlflow.schemas.base import BaseOrmConfig
 
@@ -17,3 +18,15 @@ class Model(BaseModel, BaseOrmConfig):
 class UpdateModel(BaseModel):
     name: str | None = None
     tags: list[str] | None = None
+
+
+class ModelDetails(Model):
+    static_params: dict[str, Any] | None = None
+    dynamic_params: dict[str, Any] | None = None
+    experiments: list[dict] = Field(default_factory=list)
+
+
+class ModelExperiment(BaseModel, BaseOrmConfig):
+    id = str
+    name: str
+    input: dict = Field(default_factory=dict)
