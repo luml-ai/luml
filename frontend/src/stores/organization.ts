7a41ca199e7cc7dd7d65085618b22a827f5ee5e8
Limanskiy <102548243+limanski-valera@users.noreply.github.com>
2026-05-28 12:28:34 +0300

    fix/LM-614: deduplicate requests (#557)

--- frontend/src/stores/organization.ts
+++ frontend/src/stores/organization.ts
@@ -15,6 +15,7 @@ import { useOrbitsStore } from './orbits'
 
 export const useOrganizationStore = defineStore('organization', () => {
   const availableOrganizations = ref<Organization[]>([])
+  const availableOrganizationsRequest = ref<Promise<void> | null>(null)
   const organizationDetails = ref<OrganizationDetails | null>(null)
   const currentOrganizationId = ref<string | null>(null)
   const loading = ref(false)
@@ -32,9 +33,20 @@ export const useOrganizationStore = defineStore('organization', () => {
   }
 
   async function getAvailableOrganizations() {
-    const response = await api.getOrganizations()
-    availableOrganizations.value = response
-    await setInitialOrganization()
+    if (availableOrganizationsRequest.value) return availableOrganizationsRequest.value
+    availableOrganizationsRequest.value = new Promise(async (resolve, reject) => {
+      try {
+        const response = await api.getOrganizations()
+        availableOrganizations.value = response
+        await setInitialOrganization()
+      } catch (e) {
+        reject(e)
+      } finally {
+        availableOrganizationsRequest.value = null
+      }
+    })
+
+    return availableOrganizationsRequest
   }
 
   async function createOrganization(payload: CreateOrganizationPayload) {
